a6e7e033337a3f283a012d8327d7a4f852c223f0
Daniil Sochnev <sochnevde09@gmail.com>
2025-09-04 10:10:45 +0500

    first solves

---
+++ Азбука C++/Hello, world/Classwork/3.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    cout << "После числа " << a << " следует " << a + 1 << endl;
+    cout << "Перед числом " << a << " расположено число " << a - 1 << endl;
+}
