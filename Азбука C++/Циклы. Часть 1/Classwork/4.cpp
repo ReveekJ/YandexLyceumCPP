114c498aab7039465ce5800482b46f3ecaf6da39
MushroomForYou <k.i.vetoshkin@gmail.com>
2025-09-11 09:41:05 +0500

    solve 4

---
+++ Азбука C++/Циклы. Часть 1/Classwork/4.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+signed main(void){
+    int a;
+    cin >> a;
+    int b = 1;
+    while (b <= a){
+        cout << b << " ";
+        b *= 2;
+    }
+}
