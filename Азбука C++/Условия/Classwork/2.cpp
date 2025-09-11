1b3801c5dd2997f70495a750864c6a2f1623c548
Zermd1000-7 <chahutin.dania@gmail.com>
2025-09-11 09:47:48 +0500

    Create 2.cpp

---
+++ Азбука C++/Условия/Classwork/2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    switch (a) {
+        case 1:
+        cout << "mon";
+        break;
+        case 2:
+        cout << "tue";
+        break;
+        case 3:
+        cout << "wed";
+        break;
+        case 4:
+        cout << "thu";
+        break;
+        case 5:
+        cout << "fri";
+        break;
+        case 6:
+        cout << "sat";
+        break;
+        case 7:
+        cout << "sun";
+        break;
+
+    }
+}
